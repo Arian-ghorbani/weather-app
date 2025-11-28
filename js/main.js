68805d19f3a6d119549a817675e9fa6f53af68e6
Arian Ghorbani <arianghorbani220687@gmail.com>
2025-11-28 11:39:08 +0330

    feat: Add weather API feature

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const htmlElem = document.documentElement;
 // Theme
 const themeBtn = document.querySelector("#theme-btn");
 let colorTheme = null;
+// Weather API
+const searchBtn = document.querySelector(".search-btn");
+const baseURL = "https://api.openweathermap.org/data/2.5/weather";
+const apiKey = "f2a3b99194244950f22614868d9f6217";
 
 // Theme
 const getThemeLocalStorage = () => {
@@ -30,6 +34,36 @@ const changeTheme = () => {
   saveThemeLocalStorage();
 };
 
+// Weather API
+const inputDataWeather = (city, temp, humidity, wind, pressure) => {
+  const cityElem = document.querySelector("#city-name");
+  const tempElem = document.querySelector(".temp");
+  const humidityElem = document.querySelector(".humidity");
+  const windElem = document.querySelector(".wind");
+  const pressureElem = document.querySelector(".pressure");
+
+  cityElem.innerHTML = `${city}`;
+  tempElem.innerHTML = `${temp}°C`;
+  humidityElem.innerHTML = `${humidity}%`;
+  windElem.innerHTML = `${wind} km/h`;
+  pressureElem.innerHTML = `${pressure} hpa`;
+};
+
+const getWeatherAPI = async () => {
+  const inputCityName = document.querySelector("#input-city");
+  const cityName = inputCityName.value.trim();
+  const response = await fetch(`${baseURL}?q=${cityName}&appid=${apiKey}`);
+  const data = await response.json();
+
+  const city = data.name;
+  const temp = Math.floor(data.main.temp - 273.15);
+  const humidity = data.main.humidity;
+  const wind = data.wind.speed;
+  const pressure = data.main.pressure;
+
+  inputDataWeather(city, temp, humidity, wind, pressure);
+};
+
 const pageLoaded = () => {
   // Theme
   getThemeLocalStorage();
@@ -37,4 +71,7 @@ const pageLoaded = () => {
 
 // Theme
 themeBtn.addEventListener("click", changeTheme);
+// Search API
+searchBtn.addEventListener("click", getWeatherAPI);
+
 window.addEventListener("DOMContentLoaded", pageLoaded);
