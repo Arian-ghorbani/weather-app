98fa074267fac7aac60f8f0a5276a346441511f5
Arian Ghorbani <arianghorbani220687@gmail.com>
2025-12-31 15:04:56 +0330

    feat: Add AI and loading features

--- js/main.js
+++ js/main.js
@@ -11,6 +11,13 @@ const mainContentElem = document.querySelector(".main-content");
 const infoBtn = document.querySelector("#info-btn");
 const infoContainer = document.querySelector(".info-container");
 const dateElem = document.querySelector(".date");
+const aiBtn = document.querySelector("#ai-btn");
+const chatContainerElem = document.querySelector(".chat-container");
+const closeChatBtn = document.querySelector(".closex-btn");
+const chatBodyElem = document.querySelector(".chat-body");
+const chatbotSendBtn = document.querySelector(".chatbot-send-btn");
+const chatbotInput = document.querySelector("#chatbot-input");
+const suggestionForUserElem = document.querySelector(".suggestion-for-user");
 
 // Weather info
 const cityElem = document.querySelector("#city-name");
@@ -23,26 +30,18 @@ const tempIconWrapper = document.querySelector(".temp-icon-wrapper");
 // API
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 const API_KEY = "f2a3b99194244950f22614868d9f6217";
+const AI_BASE_URL = "https://router.huggingface.co/v1/chat/completions";
+const AI_API_KEY = "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";
 
-// Animation
-const addAnimation = () => {
-  main.classList.add("opacity-100");
+let weatherData = null;
+
+// Search loader
+const showSearchLoading = () => {
+  searchBtn.innerHTML = '<iconify-icon icon="mingcute:loading-fill" class="icon animate-spin"></iconify-icon>';
 };
 
-// ====== Theme ======
-const backgroundChange = () => {
-  const h = new Date().getHours();
-
-  const theme = h >= 6 && h < 12
-    ? ["#0084D1", "#00BCFF"]
-    : h >= 12 && h < 20
-      ? ["#f54a00", "#ff8904"]
-      : h >= 20
-        ? ["#030712", "#101828"]
-        : ["#030712", "#101828"];
-
-  htmlElem.style.setProperty("--color-primary", theme[0]);
-  htmlElem.style.setProperty("--color-secondary", theme[1]);
+const hideSearchLoading = () => {
+  searchBtn.innerHTML = '<iconify-icon icon="mingcute:search-3-line" class="icon"></iconify-icon>';
 };
 
 // ====== Date ======
@@ -64,14 +63,18 @@ const hideError = () => {
   mainContentElem.classList.remove("hidden");
 };
 
-const showError = () => {
+const showError = (message) => {
   searchStatusElem.classList.remove("hidden");
-  searchStatusTitleElem.textContent = "Location not found";
+  searchStatusTitleElem.textContent = message;
   cityNameElem.classList.add("hidden");
   mainContentElem.classList.add("hidden");
 };
 
-const updateWeatherUI = ({ city, temp, humidity, wind, pressure, condition }) => {
+const updateWeatherUI = async ({ city, temp, humidity, wind, pressure, condition }) => {
+  const message = `بر اساس این اطلاعات، در چند کلمه، خیلی خلاصه و متن شکسته نباشه، به من پیشنهاد بده امروز چه نکاتی را رعایت کنم.`;
+  const suggestionForTheUser = await getAIAPI(message);
+  suggestionForUserElem.textContent = suggestionForTheUser;
+
   cityElem.textContent = city;
   tempElem.textContent = `${temp}`;
   humidityElem.textContent = `${humidity}%`;
@@ -91,20 +94,18 @@ const updateWeatherUI = ({ city, temp, humidity, wind, pressure, condition }) =>
 };
 
 const getWeatherAPI = async () => {
+  if (!navigator.onLine) return showError("You are offline");
   const city = inputCityName.value.trim();
   if (!city) return;
 
-  main.classList.remove("opacity-100");
+  showSearchLoading();
 
   try {
     const url = `${BASE_URL}?q=${city}&appid=${API_KEY}`;
     const res = await fetch(url);
-
-    if (!res.ok) return showError();
-
     const data = await res.json();
 
-    const weatherData = {
+    weatherData = {
       city: data.name,
       temp: Math.round(data.main.temp - 273.15),
       humidity: data.main.humidity,
@@ -113,15 +114,87 @@ const getWeatherAPI = async () => {
       condition: data.weather[0].main.toLowerCase(),
     };
 
-    setTimeout(() => {
-      updateWeatherUI(weatherData);
-      updateDate();
-      hideError();
-    }, 200);
+    await updateWeatherUI(weatherData);
+    updateDate();
+    hideError();
   } catch (err) {
-    showError();
+    showError("Location not found");
   } finally {
-    setTimeout(() => { addAnimation() }, 200);
+    hideSearchLoading();
+  }
+};
+
+// ====== Chatbot ======
+const createMessageElem = (message, elemClass) => {
+  const userMessage = `<div class="text-message">${message}</div>`;
+  const divElem = document.createElement("div");
+  divElem.classList.add(elemClass);
+  divElem.innerHTML = userMessage;
+  return divElem;
+};
+
+const addMessageElem = (element) => {
+  chatBodyElem.appendChild(element);
+
+  setTimeout(() => {
+    const icons = {
+      botIcon: '<iconify-icon icon="bxs:bot" class="ai-icon"></iconify-icon>',
+      messageIcon: '<iconify-icon icon="svg-spinners:3-dots-scale" class="icon"></iconify-icon>',
+    };
+    const aiMessageElem = createMessageElem(icons.messageIcon, "ai-message");
+    aiMessageElem.innerHTML += icons.botIcon;
+    chatBodyElem.appendChild(aiMessageElem);
+  }, 500);
+};
+
+
+// ====== AI API ======
+const getAIAPI = async (userMessage) => {
+  const messages = [
+    {
+      role: "system",
+      content: `تو یک دستیار هواشناسی در یک برنامه هواشناسی هستی و بر اساس اطلاعات هواشناسی امروز: شهر: ${weatherData.city}، دما: ${weatherData.temp}°C، رطوبت: ${weatherData.humidity}%، سرعت باد: ${weatherData.wind} km/h، فشار هوا: ${weatherData.pressure} hpa، وضعیت هوا: ${weatherData.condition}. به فارسی جواب میدهی و از اصطلاحات ساده، دوستانه و محاوره‌ای استفاده کن.`
+    },
+    {
+      role: "user",
+      content: userMessage
+    }
+  ];
+
+  const res = await fetch(AI_BASE_URL, {
+    headers: {
+			"Authorization": `Bearer ${AI_API_KEY}`,
+			"Content-Type": "application/json",
+		},
+		method: "POST",
+		body: JSON.stringify({
+      messages,
+      "model": "openai/gpt-oss-120b",
+      "stream": false
+    }),
+  });
+
+  try {
+    const data = await res.json();
+    responseMessage = data.choices[0].message.content;
+    return responseMessage;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const showChatbotData = async (userMessage) => {
+  const message = await getAIAPI(userMessage);
+  chatBodyElem.lastElementChild.querySelector(".text-message").innerHTML = marked.parse(message);
+};
+
+const sentMessage = () => {
+  const userMessage = chatbotInput.value.trim();
+  if (userMessage) {
+    const userElem = createMessageElem(userMessage, "user-message");
+    addMessageElem(userElem);
+    showChatbotData(userMessage);
+    chatbotInput.value = "";
   }
 };
 
@@ -131,11 +204,23 @@ infoContainer.addEventListener("click", (e) => {
   if (e.target === infoContainer) infoContainer.classList.add("hidden");
 });
 
-// ====== Init ======
-window.addEventListener("DOMContentLoaded", () => {
-  backgroundChange();
-  addAnimation();
+// ====== Chatbot ======
+aiBtn.addEventListener("click", (e) => {
+  // Show chatbot
+  if (aiBtn && e.target.closest("#ai-btn")) chatContainerElem.classList.add("open");
 });
+
+closeChatBtn.addEventListener("click", (e) => {
+  // Hide chatbot
+  if (closeChatBtn && e.target.closest(".closex-btn")) chatContainerElem.classList.remove("open");
+});
+
+chatbotSendBtn.addEventListener("click", sentMessage);
+
+chatbotInput.addEventListener("keyup", (e) => { if (e.key === "Enter") sentMessage() });
+
+// ====== Init ======
+// window.addEventListener("DOMContentLoaded", );
 
 // ====== Events ======
 searchBtn.addEventListener("click", getWeatherAPI);
